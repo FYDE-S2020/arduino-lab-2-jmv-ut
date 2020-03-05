e24c41aa188a7cff681b51d76c4c3b3d11d9b221
jmv-ut <jonathan.voss@utexas.edu>
2020-03-04 21:24:07 -0600

    Uploading Finished Assignment

--- BlynkStarter/BlynkStarter.ino
+++ BlynkStarter/BlynkStarter.ino
@@ -32,31 +32,104 @@
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
 
-
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "Your token here";
+char auth[] = "7JxUQKOL2nvKWMq0Mi3L4L_XeUZQC4I-";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 // The EE IOT network is hidden. You might not be able to see it.
 // But you should be able to connect with these credentials. 
-char ssid[32] = "EE-IOT-Platform-02";
-char pass[32] = "g!TyA>hR2JTy";
+char ssid[32] = "Glow Cloud";
+char pass[32] = "hailtheeer";
+const int LED = 0;
+const int LEDvpin = 0; //brightness on 1, code unclear
+const int freq = 5000;     // 5KHz frequency is more than enough. Remember we used 100 before.
+const int ledChannel = 0;  // You can have up to 16 pwm channels (0 to 15)
+const int resolution = 10; // 10 bit resolution for 1023. Freq should be greater than resolution.
+BlynkTimer timer;
+int time_count = 0; // timer counter global variable
+String content = "";  // null string constant ( an empty string )
+int ON = 0;
+int val = 0;
+
+
+void myTimerEvent() // Every 10 ms
+{
+    if (time_count == 100){
+        Blynk.virtualWrite(V2, millis() / 1000); // Write the arduino uptime every second
+        time_count = 0; // reset time counter
+    }
+    else {
+        // Send serial data to Blynk terminal
+        char character;
+        while(Serial.available()) { // Check if serial is available every 10 ms
+            character = Serial.read();
+            content.concat(character);
+        }
+        if (content != "") {
+            Blynk.virtualWrite(V3, content);
+            content = ""; // Clear String
+        }  
+    }
+    time_count += 1; // Increment on every tick
+}
+
 
 void setup()
 {
   // Serial Monitor
   Serial.begin(115200);
   Blynk.begin(auth, ssid, pass);
+  pinMode(LED, OUTPUT);
+  ledcSetup(ledChannel, freq, resolution);
+  ledcAttachPin(LED, ledChannel);
+
+   timer.setInterval(10L, myTimerEvent); // 10 ms interval
 }
 
 void loop()
 {
   Blynk.run();
+  timer.run();
 }
 
+BLYNK_WRITE(V0)
+{
+  
+    // param is a member variable of the Blynk ADT. It is exposed so you can read it.
+    int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+
+    // Because V0 is a button, pinValue will be a 0 or a 1.
+    if (pinValue == 0) {
+        ON = 0;
+        ledcWrite(ledChannel, 0);
+    }
+    else {
+        ON = 1;
+        ledcWrite(ledChannel, val);
+    }
+}
+
+BLYNK_READ(V2) // Widget in the app READS Virtal Pin V2 with the certain frequency
+{
+    // This command writes Arduino's uptime in seconds to Virtual Pin V2
+      Blynk.virtualWrite(5, millis() / 1000);
+}
+
+BLYNK_WRITE(1)
+{
+    val = param.asInt();
+    if(ON==1){
+    // param is a member variable of the Blynk ADT. It is exposed so you can read it.
+    // assigning incoming value from pin V1 to a variable (0-100 ints/
+    ledcWrite(ledChannel, val); // Note that this takes ledChannel as an argument, NOT the pin! Set duty = val.
+    }
+    else{
+      digitalWrite(LED, LOW);
+    }
+}
--- Report.md
+++ Report.md
@@ -1,43 +1,52 @@
-Name:
+Name:Jonathan Voss
 
-EID:
+EID: jmv3323
 
-Team Number:
+Team Number: 4
 
 ## Questions
 
 1. What is the purpose of an IP address?
 
-    answer
+    IP adresses give a unique name, or ID, to each device on a network to allow machines to
+	identify one another.
 
 2. What is a DNS? What are the benefits of using domain names instead of IP addresses?
 
-    answer
+    the Domain Name Service, this maps a symbolic name to its respective IP address.
+	this is great becuase a 32 bit number is difficult to memorize for humans.
 
 3. What is the difference between a static IP and a dynamic IP?
 
-    answer
+    A static IP address will not change, while Dynamic IPs can. Static is better for server/website hosting as the IP will not
+	change, but Dynamic is not only cheaper but also easier for everyday use and potentially more secure as it can change.
 
 4. What is the tradeoff between UDP and TCP protocols?
 
-    answer
-
+    While TCPs have no loss and are thus more reliable, UDP is connectionless and faster, but more unreliable.
+	
 5. Why can't we use the delay function with Blynk?
 
-    answer
+    Blynk is event based, meaning it must use timing to determine actions,, and a delay would
+	mess with the timing and break the code.
 
 6. What does it mean for a function to be "Blocking"?
 
-    answer
+    A blocking function will not allow any other code to run while it is running- it "blocks" all other code
+	from executing.
 
 7. Why are interrupts useful for writing Non-Blocking code?
 
-    answer
+    interrupts allow code to excecute in the middle of what would be "blocking" code, allowing the code to run 
+	at seperate times, rendering the code non-blocking and allowing the processor to choose (based on priority) what
+	code to excecute.
 
 8. What is the difference between interface and implementation? Why is it important?
 
-   answer
+   interface decides how a function is called/accessed (and is usually in the header file), while
+   implementation defines what the code does when it is called. This is important because they are 
+   seperate. In Blynk, they define the interface while we, the programmer, decide the implimentation.
 
 9. Screenshot of your Blynk App:
 
-    ![your image here->](img/placeholder.png)
+    ![Blynk app SS](img/BLYNKSS.png)
